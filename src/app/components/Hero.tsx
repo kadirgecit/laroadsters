b5a7a425a1f49bb2db95ee42acbea89093761b4c
Kadir Gecit <kadirgecit@gmail.com>
2026-05-29 07:35:56 +0000

    Fix: Increase hero padding to push image below navbar

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -107,7 +107,7 @@ export function Hero() {
   };
 
   return (
-    <div ref={heroRef} className="relative h-screen overflow-hidden bg-black pt-16">
+    <div ref={heroRef} className="relative h-screen overflow-hidden bg-black pt-24">
       {/* Background Image */}
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
