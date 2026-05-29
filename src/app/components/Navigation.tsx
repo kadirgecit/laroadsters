b9347c551e3ac222d8d8f5846d70bff253d8f7fe
Kadir Gecit <kadirgecit@gmail.com>
2026-05-29 10:46:44 +0000

    Make navbar logo larger in oval shape

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -64,7 +64,9 @@ export function Navigation() {
           <div className="flex items-center justify-between">
             {/* Logo */}
             <Link to="/" className="group">
-              <img src="/la-roadsters-inverted.png" alt="LA Roadsters" className="h-12" />
+              <div className="h-16 w-32 rounded-full overflow-hidden border-2 border-red-500 shadow-lg">
+                <img src="/la-roadsters-inverted.png" alt="LA Roadsters" className="w-full h-full object-cover" />
+              </div>
             </Link>
 
             {/* Desktop Navigation */}
