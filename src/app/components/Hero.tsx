b9ab9a9ce7879967babf9b1d93b7b632bef11305
Kadir Gecit <kadirgecit@gmail.com>
2026-05-29 11:05:46 +0000

    Hero: shift image down so text not behind navbar

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -109,8 +109,8 @@ export function Hero() {
   return (
     <div ref={heroRef} className="relative h-screen overflow-hidden bg-black">
       <div
-        className="absolute inset-0 bg-cover bg-center bg-[length:150%] md:bg-[length:100%]"
-        style={{ backgroundImage: 'url(/hero-bg.png)' }}
+        className="absolute inset-0 bg-cover bg-[length:150%] md:bg-[length:100%]"
+        style={{ backgroundImage: 'url(/hero-bg.png)', backgroundPosition: '50% 20%' }}
       />
 
       {/* Main Content */}
