35c88482f4b38e2c2ef1f881c0c667de97b99153
Kadir Gecit <kadirgecit@gmail.com>
2026-06-02 06:07:10 +0000

    Replace hero image with LAROADSTERS.jpg

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -110,7 +110,7 @@ export function Hero() {
     <div ref={heroRef} className="relative h-screen overflow-hidden bg-black">
       <div
         className="absolute inset-0 bg-cover bg-no-repeat bg-[length:150%] md:bg-[length:100%]"
-        style={{ backgroundImage: 'url(/hero-bg.png)', backgroundPosition: '50% 30%' }}
+        style={{ backgroundImage: 'url(/hero-bg.jpg)', backgroundPosition: '50% 30%' }}
       />
 
       {/* Main Content */}
